ad2356fefa5798d5784ec9810018ec40df7c9ab1
Mazen Sharkawy <mazenmaher1997@gmail.com>
2021-01-14 18:49:58 +0200

    SEO: H1 title, meta title, meta description

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from "./logo.svg";
+import React from "react";
 import GameBoard from "./containers/GameBoard";
-
+import styled from "styled-components";
+const MainContainer = styled.div`
+display: flex;
+flex-direction: column;
+`
+const Title = styled.h1`
+  margin: 0 auto;
+`;
 function App() {
-  return <GameBoard />;
+  return (
+    <MainContainer>
+      <Title>Cool Memory Game</Title>
+      <GameBoard />
+    </MainContainer>
+  );
 }
 
 export default App;
